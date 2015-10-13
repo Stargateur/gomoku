0f7eddf8d9b84c14fa1ad6cea672c34d9b76c0fa
zwertv_e <zwertv_e@epitech.eu>
2015-10-13 11:58:26 +0200

    Init GUI folder

---
+++ GUI/source.mk
@@ -0,0 +1,13 @@
+##
+## source.mk for  in /home/elliott/rendu/gomoku
+## 
+## Made by Elliott
+## Login   <zwertv_e@epitech.net>
+## 
+## Started on  Tue Oct 13 11:52:28 2015 Elliott
+## Last update Tue Oct 13 11:57:36 2015 Elliott
+##
+
+PATH_SRC	=	src/
+
+SRC		+=	$(PATH_SRC)main.cpp
---
+++ GUI/src/main.cpp
@@ -0,0 +1,14 @@
+//
+// main.cpp for  in /home/elliott/rendu/gomoku/GUI/src
+// 
+// Made by Elliott
+// Login   <zwertv_e@epitech.net>
+// 
+// Started on  Tue Oct 13 11:54:05 2015 Elliott
+// Last update Tue Oct 13 11:54:17 2015 Elliott
+//
+
+int		main(void)
+{
+  return (0);
+}
