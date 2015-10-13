045399e01da368142c0c187dcb518a9d16bfcbe4
zwertv_e <zwertv_e@epitech.eu>
2015-10-13 17:58:16 +0200

    first image

--- GUI/src/main.cpp
+++ GUI/src/main.cpp
@@ -5,11 +5,33 @@
 // Login   <zwertv_e@epitech.net>
 // 
 // Started on  Tue Oct 13 13:26:35 2015 Elliott
-// Last update Tue Oct 13 14:41:02 2015 Elliott
+// Last update Tue Oct 13 17:57:58 2015 Elliott
 //
 
 #include	<iostream>
 #include	<SDL2/SDL.h>
+
+void		print_button(SDL_Surface *dest)
+{
+  SDL_Surface	*surface;
+  SDL_Rect	src_rec;
+  SDL_Rect	dest_rec;
+
+  if (dest == NULL)
+    std::cerr << "no surface to copy." << std::endl;
+  surface = SDL_LoadBMP("img/jouer.bmp");
+  if (surface == NULL)
+    std::cerr << "load img failed." << std::endl;
+  src_rec.x = 0;
+  src_rec.y = 0;
+  src_rec.h = 128;
+  src_rec.w = 128;
+  dest_rec.x = 50;
+  dest_rec.y = 150;
+  dest_rec.h = 32;
+  dest_rec.w = 32;
+  SDL_BlitScaled(surface, &src_rec, dest, &dest_rec);
+}
 
 int		create_win(void)
 {
@@ -23,7 +45,7 @@ int		create_win(void)
       return (1);
     }
 
-  win = SDL_CreateWindow("Title", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_OPENGL);
+  win = SDL_CreateWindow("Title", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 300, 400, SDL_WINDOW_OPENGL);
 
   if (win == NULL)
     {
@@ -35,18 +57,17 @@ int		create_win(void)
   render = SDL_CreateRenderer(win, -1, 0);
   SDL_SetRenderDrawColor(render, 0, 0, 0, 1);
   SDL_RenderClear(render);
-  SDL_RenderPresent(render);
+  print_button(SDL_GetWindowSurface(win));
+  SDL_UpdateWindowSurface(win);
 
   while (!end)
     {
       SDL_Event event;
-      if (SDL_WaitEvent(NULL) && SDL_HasEvent(SDL_KEYDOWN) == true)
+      SDL_WaitEvent(&event);
+      if (event.type != SDL_KEYDOWN)
 	{
-	  while (SDL_PollEvent(&event))
-	    {
-	      if (event.key.keysym.sym == SDLK_ESCAPE)
-		end = true;
-	    }
+	  if (event.key.keysym.sym == SDLK_ESCAPE)
+	    end = true;
 	}
     }
 
