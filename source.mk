c18991f81a68331e2f53df272e818c8ef10fcfd5
zwertv_e <elliott.zwertvaegher@epitech.eu>
2016-02-08 16:15:24 +0100

    PlayerInfo now singleton

--- source.mk
+++ source.mk
@@ -50,3 +50,4 @@ SRC_SERVER	+=	$(DIR_SERVER)/Black.cpp
 SRC_CLIENT	=	$(DIR_CLIENT)/main.cpp
 SRC_CLIENT	+=	$(DIR_CLIENT)/Client.cpp
 SRC_CLIENT	+=	$(DIR_CLIENT)/PlayerInfo.cpp
+SRC_CLIENT	+=	$(DIR_CLIENT)/GameInfo.cpp
